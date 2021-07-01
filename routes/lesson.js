e8678bda11b6956700d04995db9ea5036b1e555c
Alifalfa73 <champuter@DESKTOP-2J524DD.localdomain>
2021-07-01 07:30:21 +0700

    Add more endpoint, along with placeholder query and parameter

--- routes/lesson.js
+++ routes/lesson.js
@@ -2,8 +2,56 @@ const express = require('express');
 const router = express.Router();
 
 
+//Create Lesson
+router.post('/', async (req, res) => {
+    console.log(req.body);
+    
+    console.log("Create Lesson");
+
+    return res.status(204).end();
+});
+
+//Update Lesson
+router.put('/', async (req, res) => {
+    console.log(req.body);
+    
+    console.log("Update Lesson");
+
+    return res.status(204).end();
+});
+
+//Get All Lesson
+// Sort Lesson (by title or date)
 router.get('/', async (req, res) => {
-    console.log("Lesson")
+    console.log(req.query);
+
+    console.log("Get Lesson");
+
+    return res.status(204).end();
+});
+
+//Get Lesson By Id
+router.get('/:id', async (req, res) => {
+    console.log(req.params);
+
+    console.log("Get Lesson");
+
+    return res.status(204).end();
+});
+
+router.delete('/', async (req, res) => {    
+    console.log("Delete Lesson");
+
+    return res.status(204).end();
+});
+
+//Create Lesson
+router.post('/publish', async (req, res) => {
+    console.log(req.body);
+    
+    console.log("Publish Lesson");
+
+    return res.status(204).end();
 });
 
 module.exports = router;
--- routes/type.js
+++ routes/type.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+// Create type (article, webinar, course)
+// Update type
+// Delete type
 
-router.get('/', async (req, res) => {
-    console.log("Type")
+router.post('/', async (req, res) => {
+    console.log("Create Type")
+
+    console.log(req.body);
+
+    return res.status(204).end();
+});
+
+router.put('/', async (req, res) => {
+    console.log("Update Type")
+
+    console.log(req.body);
+
+    return res.status(204).end();
+});
+
+router.delete('/', async (req, res) => {
+    console.log("Delete Type")
+
+    return res.status(204).end();
 });
 
 module.exports = router;
